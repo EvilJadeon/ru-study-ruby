f474b08e5abec0272549377c83c2042fc2935947
EvilJadeon <thejadeonwot@gmail.com>
2023-11-26 21:23:51 +0300

    Done binary search for Array with recirsion; Done replace method for Array

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,30 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        new_array = []
+        max_element = array.sort.last
+
+        for i in 0..array.length - 1
+          modified_element = array[i].positive? ? max_element : array[i]
+          new_array << modified_element
+        end
+        new_array
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query, low = 0, high = array.length - 1)
+        if low <= high
+          mid = (low + high) / 2
+          mid_value = array[mid]
+
+          if mid_value == query
+            return mid
+          elsif mid_value > query
+            return search(array, query, low, mid - 1)
+          elsif mid_value < query
+            return search(array, query, mid + 1, high)
+          end
+        end
+        -1
       end
     end
   end
