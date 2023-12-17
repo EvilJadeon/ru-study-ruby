c6287b4bd95e7f5c0ae0e506661e7615bcf85888
EvilJadeon <thejadeonwot@gmail.com>
2023-12-17 19:28:57 +0300

    Fixing mistakes and code optimizing

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,34 +5,35 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each
-        for i in self
-          yield i
+      def my_each(index = 0, &block)
+        if index < self.length
+          yield self[index]
+          my_each(index + 1, &block)
         end
         self
       end
 
       # Написать свою функцию my_map
       def my_map
-        my_reduce(Exercise::Fp2::MyArray.new([])) do |result, i|
+        my_reduce(MyArray.new([])) do |result, i|
           result << yield(i)
         end
       end
 
       # Написать свою функцию my_compact
       def my_compact
-        my_reduce(Exercise::Fp2::MyArray.new([])) do |result, i|
+        my_reduce(MyArray.new([])) do |result, i|
           i.nil? ? result : result << i
         end
       end
 
-      # Написать свою функцию my_reduce
-      def my_reduce(acc_init = nil)
-        acc = acc_init
-        for i in self
-          acc = acc.nil? ? i : yield(acc, i)
+      def my_reduce(accumulator = nil, index = 0, &block)
+        if index < self.length
+          new_accumulator = accumulator.nil? ? self[index] : yield(accumulator, self[index])
+          my_reduce(new_accumulator, index + 1, &block)
+        else
+          accumulator
         end
-        acc
       end
     end
   end
